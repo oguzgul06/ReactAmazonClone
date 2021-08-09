6133ae9e4cb6441a0c844bd8cf9a96d5ccb2618d
oguz gül <ogzgl95@outlook.com>
2021-08-09 11:21:35 +0300

    Build React-Router on the Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Amozon Clone Website</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+              <h1>Checkout</h1>
+          </Route>
+          <Router path="/login">
+              <h1>Login page</h1>
+          </Router>
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
